33a4a025af8be73a9d0259fbc6ea31f48b0497ec
qoudri4re <qoudri4re@gmail.com>
2022-03-17 23:38:08 +0100

    deployed to heroku

--- CRUD/DB/connection.php
+++ CRUD/DB/connection.php
@@ -1,8 +1,8 @@
 <?php 
-        define ("DB_HOST","localhost");
-        define ("DB_USERNAME","root");
-        define ("DB_PASSWORD","qoudri4re");
-        define ("DB_NAME","crud");
+        define ("DB_HOST","sql4.freemysqlhosting.net");
+        define ("DB_USERNAME","sql4479573");
+        define ("DB_PASSWORD","KNM9akiFKj");
+        define ("DB_NAME","sql4479573");
     
         $connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
         mysqli_set_charset($connection, 'utf8');
